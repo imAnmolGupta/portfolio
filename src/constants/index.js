1fb0de36ba22876d1094c360b1f27f31390aef6e
imAnmolGupta <anmolgupta682@gmail.com>
2024-12-07 15:26:14 +0530

    Added Projects section

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,6 @@
 import project1 from "../assets/projects/project-1.png";
-//import project2 from "../assets/projects/project-2.jpg";
-//import project3 from "../assets/projects/project-3.jpg";
+import project2 from "../assets/projects/project-2.png";
+import project3 from "../assets/projects/project-3.png";
 
 export const HERO_CONTENT = `I am a dedicated Full Stack Developer with a talent for building robust and scalable web applications. With 3 years of practical experience, I have developed expertise in front-end technologies such as React.js, as well as back-end systems utilizing Node.js and MongoDB. My mission is to apply my skills to create innovative solutions that enhance business performance and deliver outstanding user experiences`;
 export const ABOUT_TEXT = `I am a versatile and dedicated Full Stack Developer passionate about building efficient and user-centric web applications. Over the past 3 years, I have gained extensive experience with technologies like React.js, Node.js, Express.js and MongoDB. My journey in web development started with a curiosity about how things work, evolving into a fulfilling career where I embrace learning and tackling new challenges. I excel in collaborative settings and enjoy solving complex problems to deliver exceptional solutions. `;
@@ -23,27 +23,28 @@ export const EXPERIENCES = [
 ];
 
 export const PROJECTS = [
-  {
-    title: "E-Commerce Website",
-    image: project1,
-    description: 
-      "A fully functional social media web application allowing users to create profiles, post content, comment, and interact securely and efficiently. It features secure authentication and responsive design.",
-    technologies: ["Node.js", "Express.js", "Docker", "MVC Architecture"],
-
-  },
+  
   {
     title: "Hotel Booking System",
     image: project1,
     description: 
       "A full-stack hotel booking system with features like user authentication, searching, filtering, and an admin panel. The application includes interactive and responsive user interfaces and efficient back-end API integrations.",
     technologies: ["Node.js", "Express.js", "React.js", "MongoDB", "Tailwind CSS", "Redux"],
-},
+  },
+  {
+    title: "CodeSocial - Social Media WebApp ",
+    image: project2,
+    description: 
+      "A fully functional social media web application allowing users to create profiles, post content, comment, and interact securely and efficiently. It features secure authentication and responsive design.",
+    technologies: ["Node.js", "Express.js","Mongoose" ,"Docker", "MVC Architecture"],
+
+  },
   {
     title: "Portfolio Website",
-    image: project1,
+    image: project3,
     description:
       "A personal portfolio website showcasing projects, skills, and contact information.",
-    technologies: ["HTML", "CSS", "React", "Tailwind"],
+    technologies: ["HTML", "CSS", "React", "Tailwind","Framer motion"],
   },
  
 ];
